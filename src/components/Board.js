7f93a2f57faeb9597b0230e6f934d830727b43ee
fumzy123 <omafumeu@gmail.com>
2023-03-08 22:42:15 -0330

    Added check winner functionality

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,44 +1,68 @@
 // Library
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 // CSS
 import '../css/Board.css'
 
 export default function Board(){
-    
+
+    // player 1 = RED and player 2 = BLUE
+    const players = [
+        {
+            name : "player1",
+            color: "red"
+        },
+
+        {
+            name : "player2",
+            color: "blue"
+        }
+    ]
+
+
+    //--------------------------------------------------------------
     // State
     const [boxColors, setBoxColors] = useState(
         [
-            {id: "1", color: ""}, 
+            {id: "0", color: ""}, 
+            {id: "1", color: ""},
             {id: "2", color: ""},
             {id: "3", color: ""},
             {id: "4", color: ""},
             {id: "5", color: ""},
             {id: "6", color: ""},
             {id: "7", color: ""},
-            {id: "8", color: ""},
-            {id: "9", color: ""}
+            {id: "8", color: ""}
         ]
     )
-    const [activePlayer, setActivePlayer] = useState(1)
+    // const [activePlayer, setActivePlayer] = useState(1)
+    const [gameState, setGameState] = useState(
+        {
+            activePlayer: players[0].name,
+            winner: ""
+        }
+    )
 
+    console.log(gameState);
     
 
-    // Event Handler: player 1 = RED and player 2 = BLUE
+    // Event Handler: 
     function handleClick(event){
+        
+        // If there is already a winner don't bother
+        if (!(gameState.winner == "")){return}
 
-        // Get the id of the box
-        const boxId = event.target.id
-        console.log(boxId)
+        // Get boxId and background Color of div
+        const {id, style} = event.target;
+        const boxId = id
+        const boxCurrentColor = style.backgroundColor
 
-        // Get the color
-        const currentColor = event.target.style.backgroundColor
-
+        
         // Change color only if it is blank
-        if (currentColor == ""){
+        if (boxCurrentColor == ""){
       
-            // Decide Color to change it to based on the active player
-            const divColor = activePlayer == 1 ? "red" : "blue"
+            // Get the active Player's color
+            const activePlayerColor = players.find(player => player.name == gameState.activePlayer).color;
 
             // Use the id to update that particular box
             setBoxColors(prevBoxColors => {
@@ -46,34 +70,24 @@ export default function Board(){
                 const newBoxColors = [...prevBoxColors]
 
                 // Identify the box that needs its color updated
-                const selectedBox = newBoxColors.find((box) => {
-                    return box.id == boxId
-                })
+                const selectedBox = newBoxColors.find(box => box.id == boxId)
 
-                // Update it
-                selectedBox.color = divColor
+                // Update box color
+                selectedBox.color = activePlayerColor
 
                 return newBoxColors
             })
 
-            // Change the Active Player
-            setActivePlayer(prevActivePlayer => {
-                let newActivePlayer = prevActivePlayer;
-                if(prevActivePlayer == 1)
-                {
-                    newActivePlayer = 2
+            // Update the game state (Switch to the next player, and )
+            setGameState(prevGameState => {
+                return {
+                    ...prevGameState, 
+                    activePlayer: prevGameState.activePlayer == players[0].name ? players[1].name : players[0].name
                 }
-                else if(prevActivePlayer == 2)
-                {
-                    newActivePlayer = 1
-                }
-
-                return newActivePlayer
             })
         }
-        
-    }
 
+    }
 
     // Data Mapping: Represent box states as JSX div elements
     const boxElements = boxColors.map((box) =>{
@@ -88,6 +102,53 @@ export default function Board(){
                 </div>
     })
 
+    // --------------------------------------------------------------
+    
+    
+    // Possible Winning comnbinations
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],       // horizontal columns
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],       // vertical columns
+        [0, 4, 8], [2, 4, 6]                   // diagonal lines
+    ]
+
+    // Algorithim to check winner
+    function checkWinner(){
+        for(let winCombo of winningCombos){
+
+            // Get the index of the boxes
+            const [boxA, boxB, boxC] = winCombo
+
+            // Check if they are all set to the same color
+            if(boxColors[boxA].color && boxColors[boxA].color == boxColors[boxB].color && boxColors[boxB].color == boxColors[boxC].color){
+                console.log(boxColors[boxA].color)
+                // Return the winning color
+                return boxColors[boxA].color
+            }
+        }
+
+        return null
+    }
+
+     // Effects
+     useEffect(() => {
+
+        // Check winner
+        const winningColor = checkWinner()
+
+        // Return if there is no winner
+        if (winningColor == null){return}
+
+        // If there is a winner {Get the winner's name}
+        const winner = players.find( player => player.color == winningColor).name
+
+        // Update the Game State with the winner
+        setGameState(prevGameState => {
+            return {...prevGameState, winner: winner}
+        })
+
+        //  
+    }, [boxColors])
 
 
     // Render component
@@ -95,7 +156,7 @@ export default function Board(){
         <div className='grid'>
             
             {boxElements}
-
+            {gameState.winner && <p>{gameState.winner} won the game</p>}
         </div>
     )
 }
